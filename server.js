40584f7ccaaec5581b17d834f634624ed1ea0f20
Pedro Peralva <pedro.peralva@zooxsmart.com>
2022-03-23 17:14:57 -0300

    x

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const serverStatic = require("serve-static");
+const serveStatic = require("serve-static");
 const path = require("path");
-
 const app = express();
-
-app.use("/", serverStatic(path.join(__dirname, "/dist")));
-
-app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
-});
-
-const port = process.env.PORT || 8080;
+app.use(serveStatic(path.join(__dirname, "dist")));
+const port = process.env.PORT || 80;
 app.listen(port);
-console.log(`app is listening on port: ${port}`);
